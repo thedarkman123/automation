46b48f657adeeffdf991fcaf722812bfa663c2d8
benny refaelov <bennyrefaelov@gmail.com>
2018-11-11 16:25:49 +0200

    base Test Example

--- src/test/java/testcases/BaseTest.java
+++ src/test/java/testcases/BaseTest.java
@@ -1,5 +1,20 @@
 package testcases;
 
-public class BaseTest {
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
+import utilities.WebDriverWrapper;
+
+public class BaseTest {
+	WebDriverWrapper driverWrapper;
+	//initializations goes here
+	@BeforeSuite
+	public void setup() {
+		
+	}
+	
+	@AfterSuite
+	public void quit() {
+		
+	}
 }
