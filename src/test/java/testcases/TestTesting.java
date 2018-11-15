cd0aafec0782a2d50551225dbaaa592fc4b0b680
hackeru <hackeru@C19S8.hackeru1.local>
2018-11-15 12:01:52 +0200

    adding listener template

--- src/test/java/testcases/TestTesting.java
+++ src/test/java/testcases/TestTesting.java
@@ -1,5 +1,7 @@
 package testcases;
 
+import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import dataprov.Dataproviders;
@@ -8,8 +10,38 @@ import pageobjects.PageObject;
 public class TestTesting extends BaseTest {
 	
 	@Test(dataProvider="excelData",dataProviderClass=Dataproviders.class)
-	public void firstTest(String firstName) {
-		System.out.println(firstName);
+	public void firstTest(String firstName,String lastName) {
+		System.out.println("firstname: "+firstName);
+		System.out.println("lastname: "+lastName);
+//		log.debug("initiate site entering");
+//		PageObject.btcGainRegistration.openUrl();
+//		try {
+//			Thread.sleep(3000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+	
+	@Test
+	public void secondsTest() {
+		System.out.println("test2");
+		Assert.fail();
+//		log.debug("initiate site entering");
+//		PageObject.btcGainRegistration.openUrl();
+//		try {
+//			Thread.sleep(3000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+	
+	@Test
+	public void seconds3() {
+		System.out.println("test3");
+		Reporter.log("shalom mother fucker");
+		Assert.assertEquals(true, true);
 //		log.debug("initiate site entering");
 //		PageObject.btcGainRegistration.openUrl();
 //		try {
